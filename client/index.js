2356a5daf2cf981d84e3ea692c68938cff5af483
EC2 Default User <ec2-user@ip-172-31-46-169.us-west-2.compute.internal>
2016-07-29 07:34:31 +0000

    webpack added

---
+++ client/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+ 
+ReactDOM.render(<div>Hello world from React</div>, document.getElementById('app'));
---
+++ scripts/build.js
@@ -0,0 +1,1 @@
+webpack ./client/index.js ./bundle.js --module-bind 'js=babel?{"presets": ["es2015","react"]}'
